75cd58bfcb982b2ddf5bfa96eb53d08b699f84bc
Mutturaj Badiger <mutturajbadiger@Mutturajs-MacBook-Air-2.local>
2026-06-24 11:48:37 +0530

    Add premium homepage styling

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Website loader");
+const header = document.querySelector(".site-header");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("is-scrolled", window.scrollY > 20);
+});
